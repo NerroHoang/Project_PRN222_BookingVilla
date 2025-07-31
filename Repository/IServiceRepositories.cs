895969d851685b765091ae63f31489eaa99de080
huyne11 <huypvde170252@fpt.edu.vn>
2025-07-31 21:52:30 +0700

    Add repository

---
+++ Repository/IServiceRepositories.cs
@@ -0,0 +1,22 @@
+﻿using BussinessObject;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public interface IServiceRepositories
+    {
+        List<Service> GetAllService();
+        void AddService(Service service);
+        Service GetServiceById(int idService);
+        List<Service> GetServiceByName(string name);
+        int EditService(Service service);
+        void DeleteService(int idService);
+        public List<Service> GetAllServicePagination(int index);
+        public int GetNumberTotalService();
+        public int GetNumberService();
+    }
+}
---
+++ Repository/ServiceRepositories.cs
@@ -0,0 +1,32 @@
+﻿using BussinessObject;
+using DataAccess;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Repository
+{
+    public class ServiceRepositories : IServiceRepositories
+    {
+        private readonly ServiceDAO _serviceDAO;
+
+        public ServiceRepositories(BookingVillaPrnContext context)
+        {
+            _serviceDAO = new ServiceDAO(context);
+        }
+        public List<Service> GetAllService() => _serviceDAO.GetAllService();
+        public void AddService(Service service) => _serviceDAO.AddService(service);
+        public Service GetServiceById(int idService) => _serviceDAO.GetServiceById(idService);
+        public List<Service> GetServiceByName(string name) => _serviceDAO.GetServiceByName(name);
+        public int EditService(Service service) => _serviceDAO.EditService(service);
+        public void DeleteService(int idService) => _serviceDAO.DeleteService(idService);
+
+        public List<Service> GetAllServicePagination(int index) => _serviceDAO.GetAllServicePagination(index);
+
+        public int GetNumberTotalService() => _serviceDAO.GetNumberTotalService();
+
+        public int GetNumberService() => _serviceDAO.GetNumberService();
+    }
+}
